7eeb9ab8145bcfdc769503701355283b4ada820b
Davidoff2103 <davidoff.d777@gmail.com>
2021-05-07 11:56:09 +0200

    Pruebas

--- frontend/UI.js
+++ frontend/UI.js
@@ -1,49 +1,46 @@
-import BookService from './services/BookService';
-const bookService = new BookService();
+import LoginService from './services/LoginService';
+const loginService = new LoginService();
 
 import { format } from 'timeago.js';
 
 class UI {
 
-  async renderBooks() {
-    const books = await bookService.getBooks();
-    const booksCardContainer = document.getElementById('books-cards');
-    booksCardContainer.innerHTML = '';
-    books.forEach((book) => {
+  async renderLogins() {
+    const logins = await loginService.getLogins();
+    const loginsCardContainer = document.getElementById('logins-cards');
+    loginsCardContainer.innerHTML = '';
+    logins.forEach((login) => {
       const div = document.createElement('div');
       div.className = 'animated fadeInRight';
       div.innerHTML = `
       <div class="card m-2">
         <div class="row no-gutters">
-            <div class="col-md-4">
-                <img src="${book.imagePath}" class="img-fluid" alt="">
-            </div>
             <div class="col-md-8">
                 <div class="card-block px-2">
-                    <h4 class="card-title">${book.title}</h4>
-                    <p class="card-text">${book.author}</p>
-                    <a href="#" class="btn btn-danger delete" _id="${book._id}">X</a>
+                    <h4 class="card-title">${login.email}</h4>
+                    <p class="card-text">${login.password}</p>
+                    <a href="#" class="btn btn-danger delete" _id="${login._id}">X</a>
                 </div>
             </div>
         </div>
         <div class="card-footer w-100 text-muted">
-          ${format(book.created_at)}
+          ${format(login.created_at)}
         </div>
       </div>
       `;
-      booksCardContainer.appendChild(div);
+      loginsCardContainer.appendChild(div);
     });
   }
 
-  async addANewBook(book) {
-    await bookService.postBook(book);
-    this.renderBooks();
-    this.clearBookForm();
+  async addANewLogin(login) {
+    await loginService.postLogin(login);
+    this.renderLogins();
+    this.clearLoginForm();
   }
 
-  clearBookForm() {
-    document.getElementById('book-form').reset();
-    document.getElementById('title').focus();
+  clearLoginForm() {
+    document.getElementById('login-form').reset();
+    document.getElementById('email').focus();
   }
 
   renderMessage(message, colorMessage, secondsToRemove) {
@@ -55,17 +52,17 @@ class UI {
     div.appendChild(document.createTextNode(message));
     // Puting in the documnet
     const container = document.querySelector('.col-md-4');
-    const bookForm = document.querySelector('#book-form');
-    container.insertBefore(div, bookForm);
+    const loginForm = document.querySelector('#login-form');
+    container.insertBefore(div, loginForm);
     // Removing the div after some secconds
     setTimeout(() => {
       document.querySelector('.message').remove();
     }, secondsToRemove);
   }
 
-  async deleteBook(bookId) {
-    await bookService.deleteBook(bookId);
-    this.renderBooks();
+  async deleteLogin(loginId) {
+    await loginService.deleteLogin(loginId);
+    this.renderLogins();
   }
 
 }
